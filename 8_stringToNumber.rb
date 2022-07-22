d35f8d5e2b6362847b57fd3ed9adfc7b7f4ee9e8
le duong hien thong <michaeldiavolos@gmail.com>
2022-07-22 17:00:23 -0400

    added ruby ex 1-18

---
+++ 8_stringToNumber.rb
@@ -0,0 +1,7 @@
+print "this is a number: "
+number = gets.chomp
+print "this is a convertedNumber: "
+convertedNumber = gets.chomp.to_i
+
+puts "the class for number is #{number.class}"
+puts "the class for covertedNumber is #{convertedNumber.class}"
