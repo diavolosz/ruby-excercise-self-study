373cedddec45a0fc361b5f165edaabee53bac52c
le duong hien thong <michaeldiavolos@gmail.com>
2022-07-25 13:37:19 -0400

    added initial file for project

---
+++ TwO-O-Player/player.rb
@@ -0,0 +1,25 @@
+
+
+
+
+# this class keep tracks on player score and perform deducting score action 
+class Players_score 
+  def initialize
+    @p1_score = 3
+    @p2_score = 3
+  end
+
+  def score_deduction(player_score)
+    player_score --
+  end
+end
+
+# this class keep tracks of game and prompts 
+class Game 
+
+end
+
+# this class keep tracks of the question generated 
+class question
+
+ends
